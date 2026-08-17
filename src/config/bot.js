c47969a46e4477c65df1d71e6f6d9e2e7166523b
NickBeLazy <themcheerios@gmail.com>
2026-08-17 03:36:54 -0400

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
       {
         name: "Community Support Requests", // required by Discord API, not shown in the client
         state: "",     // this is what people actually see
-        type: 2,               // Custom
+        type: 4,               // Custom
       },
     ],
   },
